1b45c4267064e58a2c7edf1e3842345320952450
Valentino Grassi <valengrassi7@gmail.com>
2026-09-02 00:30:31 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 const FRONTEND_URL =
-  process.env.FRONTEND_URL || "http://localhost:3000"
+  process.env.FRONTEND_URL || "https://mf-logistica.netlify.app/"
 
 app.use(
   cors({
